6254af351fa57958914c418e032fb9bf4ee0719c
HrNilsson <renenilsson01@gmail.com>
2014-02-18 17:40:26 +0100

    Fixed HammingWeigth

--- TIINCO/BlockCodes/HammingWeigth.m
+++ TIINCO/BlockCodes/HammingWeigth.m
@@ -2,8 +2,9 @@ function dist = HammingWeigth(G)
 % Calculate the Hamming weigth of a vector.
 s = size(G);
 dist = 0;
-temp = 0;
+
 for i = 1:s(1)
+    temp = 0;
     for j = 1:s(2)
        if G(i,j) ~= 0 
          temp = temp + 1;
