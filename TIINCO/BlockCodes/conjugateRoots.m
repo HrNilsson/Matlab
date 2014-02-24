ba27032f0e4c34455002f8f60a023d5a2c6683ae
HrNilsson <renenilsson01@gmail.com>
2014-02-24 15:46:01 +0100

    Fixed conjugateRoots

--- TIINCO/BlockCodes/conjugateRoots.m
+++ TIINCO/BlockCodes/conjugateRoots.m
@@ -7,12 +7,15 @@ b(i+1) = b(1)^(2^i);
 if degree(b(i+1)) > 2^m-2
     b(i+1) = b(i+1)/x^(2^m-1);
 end
-
-while b(i+1) ~= root
+w1 = 1;
+while w1
     i = i+1;
     b(i+1) = b(1)^(2^i);
     while degree(b(i+1)) > 2^m-2
         b(i+1) = b(i+1)/x^(2^m-1);
+    end
+    if b(i+1) == root
+       w1 = 0;
     end
 end
 b = b(1:end-1,:);
