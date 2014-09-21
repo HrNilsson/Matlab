cfaea17562451875a0fd606385bbd65933003dae
HrNilsson <renenilsson01@gmail.com>
2014-09-21 17:55:41 +0200

    Add full TIOPTI compulsory assignment

--- TIOPTI/CompulsoryAssignment/Assignment10783.m
+++ TIOPTI/CompulsoryAssignment/Assignment10783.m
@@ -92,6 +92,221 @@ eigs(Q)
 x
 
 %% Exercise 3
-% 
+% Newtons method
+%% 
+% Given the function $f(x) = \frac{1}{2}*x^2-cos(x)$.
+%%
+% 1. Sketch the graph of $f$ and find the 1. and 2. derivative of $f$:
+syms x;
+f2(x) = 0.5*x.^2-cos(x);
+ezplot(f2)
+title('f(x)');
+ylabel('f(x)');
+xlabel('x');
+%%
+% First derivative:
+g2(x) = diff(f2)
+%%
+% Second derivative:
+F2(x) = diff(g2)
+
+%%
+% 2. Use Newtons method to find the minimizer of $f$, where:
+x0 = 0.25;
+%%
+% Newtons method uses the formular $$x^{(k+1)} =
+% x^k-\frac{f'(x^k)}{f''(x^k)}$$ when both the first and second derivatives
+% are known.
+x1 = eval(x0 - g2(x0)/F2(x0))
+x2 = eval(x1 - g2(x1)/F2(x1))
+x3 = eval(x2 - g2(x2)/F2(x2))
+%%
+% Note that the accuracy
+er = abs(x3-x2)
+%%
+% satisfy the needed accuracy of $e < 10^{(-5)}$. Thus, the needed number of
+% iterations is _3_.
+
+%% Exercise 4
+% Steepest descent algorithm: Given the quadratic function from exercise 2:
+syms xx1 xx2;
+x = [xx1;xx2];
+Q = [2 -2;-2 6];
+b = [0;-3];
+f3(x) = 0.5*x.'*Q*x-x.'*b;
+%%
+% The initail starting point is:
+x0 = [1;1];
+%%
+% The gradient of $f$ is:
+g3(x) = Q*x-b;
+%%
+% To compute $x^1$ we need $$\alpha_0 = \argmin_{\alpha \geq 0}
+% f(x^0-\alpha \bigtriangledown f(x^0)) = \Phi(\alpha)$$
+syms a;
+arg = x0-a*g3(x0(1),x0(2));
+Phi0(a) = f3(arg(1),arg(2));
+%%
+% To find $\alpha_0$ we use the FONC and plot the graph of $\Phi(\alpha)$, to ensure it is a
+% minimium
+a0 = solve(diff(Phi0(a)) == 0)
+ezplot(Phi0(a))
+title('Phi2(a)');
+%%
+% Now we find $x^1$:
+x1 = x0-a0*g3(x0(1),x0(2))
+%%
+% Now we use same approach to find $x^2$ and $x^3$:
+arg = x1-a*g3(x1(1),x1(2));
+Phi1(a) = f3(arg(1),arg(2));
+a1 = solve(diff(Phi1(a)) == 0)
+ezplot(Phi1(a))
+title('Phi1(a)');
+x2 = x1-a1*g3(x1(1),x1(2))
+%%
+
+arg = x2-a*g3(x2(1),x2(2));
+Phi2(a) = f3(arg(1),arg(2));
+a2 = solve(diff(Phi2(a)) == 0)
+ezplot(Phi2(a))
+title('Phi2(a)');
+x3 = x2-a2*g3(x2(1),x2(2))
+
+%%
+% 2. Does the steepest descent algorithm converge?
+%%
+% From Theorem 8.2 in the Chong book, the steepest descent algorithm will always converge. This is also clearly evident,
+% when looking at the graph of $f(x)$:
+ezsurf(f3(xx1,xx2))
+%%
+% 3. Use a fixed-step size algorithm, what step size should be used to
+% ensure convergence?
+%%
+% Since Q is symmetric, we know that $f(x)$ converges if the following
+% inequality is satisfied: $$0 < \alpha < \frac{2}{\lambda_{max}(Q)}$$
+lambda_max  = max(eigs(Q))
+%%
+% Thus, as long as $0 < \alpha < 6.8284$ the algorithm will converge.
+
+%% Exercise 5
+% Data-fitting problem
+%%
+% 1. Calculate and write out $J(x)$
+%%
+% Since the number of samples is unknown, we calculate $J(x)$ column-wise,
+% as a function of $t_i$:
+syms yi A ti s;
+x = [A,s];
+Jc1(x) = diff(yi-A*exp(-(ti^2)/(s^2)),A)
+Jc2(x) = diff(yi-A*exp(-(ti^2)/(s^2)),s)
+
+%%
+% 2. Fill in the missing code, and plot decision variables $A$ and $\sigma$
+% as a function of iteration number.
+%%
+% Simulation dataset 
+t=(-2:.01:2);
+x_gold=[1;.1];
+y=x_gold(1)*exp(-(t).^2/x_gold(2)^2);
+y=y+.03*randn(size(y));
+figure(1)
+plot(y,'.b'); hold on;
+xlabel('ti');
+ylabel('yi');
+xx=[];
+x=x_gold+[.9;.9];
+
+for k=1:75
+ J= [               -exp(-(t).^2/x(2)^2);
+       -(2*x(1)*t.^2.*exp(-(t).^2/x(2)^2))/x(2)^3].';
+
+    f = x(1)*exp(-(t).^2/x(2)^2);
+    r=y-f;
+
+    x=x-(J.'*J)^(-1)*(J.'*r.');
+    xx=[xx,x];
+    plot(f)
+    pause(.1)
+end
+figure(2)
+plot(xx(1,:),'.r');hold on;
+plot(xx(2,:),'.g');
+xlabel('Number of iterations');
+ylabel('A and sigma values');
+
+%%
+% Note that $\sigma$ is found to be $-0.1$, whereas the function is defined
+% with $\sigma = .1$ This is the case, since $\sigma = \pm 0.1$ both solves
+% the datafitting problem, since $\sigma$ is squared everywhere it is used.
+
+%% Exercise 6
+% Conjugate gradient algorithm
+%%
+% Given the function $f$:
+syms xx1 xx2;
+x = [xx1;xx2];
+Q = [2 -2;-2 6];
+b = [0;-3];
+f4(x) = 0.5*x.'*Q*x-x.'*b;
+%%
+% The initail starting point is:
+x0 = [1;1];
+%%
+% Calculate the first 3 steps in the conjugate gradient method.
+%%
+% The gradient of $f$ is:
+gr(x) = Q*x-b;
+%%
+% Which evaluated in $x_0$ gives:
+g0 = gr(x0(1),x0(2))
+%%
+% Singe the gradient is not zero, we continue, by setting $d_0 = - g_0$ and finding $\alpha_0$:
+d0 = -g0;
+a0 = -(g0.'*d0)/(d0.'*Q*d0)
+%% 
+% Next, we use the update function:
+x1 = x0+a0*d0
+%%
+% Reevaluating the gradient:
+g1 = gr(x1(1),x1(2))
+%%
+% Next we calculate $\beta_0$:
+b0 = g1.'*Q*d0/(d0'*Q*d0)
+%%
+% Next we find the new direction:
+d1 = -g1+b0*d0
+%%
+% Now we repeat the algorithm steps by finding $\alpha_1$ and calculating $x_2$:
+a1 = -(g1.'*d1)/(d1.'*Q*d1)
+x2 = x1+a1*d1
+%%
+% Thus, again we have found the minimum.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
